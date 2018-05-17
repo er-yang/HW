64797ce433f1bcfaef2143fb7b96734ee1b09cfa
yyc <2205666018@qq.com>
2018-05-17 21:54:25 +0800

    首页实现

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
+import {PieChart, Pie} from 'recharts';
+import IceContainer from '@icedesign/container';
+import CustomBarCharts from '../../components/customBarCharts';
+import './index.css';
+import IcePanel from '@icedesign/panel';
 
+    const data02 = [{name: 'A1', value: 100},
+                    {name: 'A2', value: 300},
+                   {name: 'B1', value: 100},
+                   {name: 'B2', value: 80},
+                   {name: 'B3', value: 40},
+                   {name: 'B4', value: 30},
+                   {name: 'B5', value: 50},
+                  {name: 'C1', value: 100},
+                  {name: 'C2', value: 200},
+                   {name: 'D1', value: 150},
+                   {name: 'D2', value: 50}];
 export default class Home extends Component {
   static displayName = 'Home';
+  
 
   constructor(props) {
     super(props);
@@ -9,6 +26,26 @@ export default class Home extends Component {
   }
 
   render() {
-    return <div className="home-page" />;
+    console.log("------", data02);
+    return (<div className="home-page" >
+              <IceContainer>
+                <IcePanel status="info">
+                  <IcePanel.Header>
+                    待处理
+                  </IcePanel.Header>
+                  <IcePanel.Body>
+                    <p>hhh hhh g</p>
+                  </IcePanel.Body>
+                </IcePanel>
+                <div className="main-panel">
+                  <PieChart width={800} height={400}>
+                    <Pie data={data02} dataKey="value" nameKey="name" cx={200} cy={200} innerRadius={70} outerRadius={90} fill="#82ca9d" label/>
+                  </PieChart>
+                </div>
+                <div className="main-panel">
+                  <CustomBarCharts ></CustomBarCharts>
+                </div>
+              </IceContainer>
+           </div>)
   }
 }
